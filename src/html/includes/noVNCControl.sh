dc987aeb88c0bec791192a846cd09e421f62054d
Jacob McElroy <jacoblairm@gmail.com>
2018-11-01 14:11:45 +0800

    fix novnccontrol.sh

--- src/html/includes/noVNCControl.sh
+++ src/html/includes/noVNCControl.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-echo "noVNCControl: $@" >> /tmp/libvirt.log
-
 sleep 1
 
+port=$(virsh vncdisplay "$2"); 
+port=${port:1};
+port1=$((port + 6080));
 
- 
-if [ "$1" == "start" ]
+if [ "$1" == "start" ] # If started, spawn the noVNC daemon
 then
-	port=$(virsh vncdisplay "$2"); 
-	port=${port:1};
-	port1=$((port + 6080));
 	port2=$((port + 5900));
-	
 	/var/www/html/includes/websockify/run -D --web "/var/www/html/" "$port1" "localhost:$port2"
 fi 
 
@@ -21,5 +17,4 @@ then
 	pidarray=($pid)
 	pid=$(echo ${pidarray[6]} | sed 's|[^0-9]||g')
 	kill -15 $pid
-
 fi
